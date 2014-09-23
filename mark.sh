39ef45a4beb0ff79f6ce1fa31a7a77a2fbccfec0
@ <@>
2014-09-22 20:46:28 -0600

    commit

--- mark.sh
+++ mark.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-a='string.txt'
+O='./out/block.txt'
+
+a='./in/string.txt'
 b=$(< $a)
 c=$(echo $b | tr ' ' '\n')
 
-echo "$c" > block.txt # unique name and mapping
+echo "$c" > $O # unique name and mapping
 
 # #!clear $VARS
 
-a='string.txt'
+a='./in/string.txt'  ## match
 #b=$(cat $a | tr [:graph:] '.' | tr [:blank:] '|' | tr '\n' '*' | tr -s '.' | tr -s '.' ' ')
 #b=$(cat $a | tr [:graph:] '.' | tr [:blank:] '|' | tr '\n' '*' | tr -s '.' | tr '.' '\n' | sed /^$/d)
 #b=$(cat $a | tr [:graph:] '.' | tr [:blank:] '|' | tr '\n' '*' | tr -s '.' | tr '.' '\n')
 #b=$(cat $a | tr [:blank:] '|' | tr '\n' '*') #reduced to expand (mark.sh | count.sh) into (mark.sh | graph | count.sh)
+
 b=$(cat $a | tr [:graph:] '.' | tr [:blank:] '|' | tr '\n' '*') # [:graph:] needed to transform before addition, unless addition is readable or guessed
 
 echo "$b"
